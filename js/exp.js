4c850fa7d19e167a5bcaec9938b1e9bad5e204b5
Sreeraj201 <sreeraj201@gmail.com>
2016-11-30 19:56:59 -0600

    added vis 2

---
+++ js/exp.js
@@ -0,0 +1,370 @@
+function mainmapcall(){
+
+        var transform = d3.geo.transform({point: projectPoint}),
+            path = d3.geo.path().projection(transform);
+
+        // get features
+        var feature = g.selectAll("path")
+            .data(geodata.features)
+            .enter();
+
+        // create paths
+        var paths = feature.append("path");
+
+        var data = new Array(24);
+        // create dictionary (effecient lookup) 
+        for(var i = 0; i < 24; i++){
+          data[i] = [];
+          if(final_list[i]){
+            for(var j = 0; j < final_list[i].length;j++){
+            data[i].push(final_list[i][j].features);
+            }
+          }
+          
+        }
+
+        // create centroid lat and long coordinates
+        circledata.objects.forEach(function(d) {
+              d.LatLng = new L.LatLng(d.circle.coordinates[1],
+                          d.circle.coordinates[0])
+            });
+            
+          // create centroid circles
+          var circlefeat = g.selectAll(".regioncenter")
+            .data(circledata.objects)
+            .enter().append("circle")
+            .style("stroke", "black")
+            .attr('class',"regioncenter")  
+            .style("opacity", .8) 
+            .style("fill", "#CD5B45")
+            .attr("r", 2); 
+
+          // create lines
+          var toLine = d3.svg.line()
+                    .interpolate("linear")
+                    .x(function(d) {
+                        return applyLatLngToLayer(d).x
+                    })
+                    .y(function(d) {
+                        return applyLatLngToLayer(d).y
+                    });
+
+        //zone filter 
+        var selected_zone = 0;
+        var zone_toggle = false;
+        
+         d3.selectAll('path').on('click',function(d){
+              
+              selected_zone = d.properties.TAZ_ID;
+                // show everything
+                if(zone_toggle){
+                  d3.selectAll('path').style('fill','#000');
+                  d3.selectAll('.travelMarker').classed('hidden',false);
+                  zone_toggle = false;
+                }
+                // hide everything except selected zone
+                else{
+                  d3.select(this).style('fill','darkblue');
+                  d3.selectAll('.travelMarker').classed('hidden',function(d){
+                    if((d[0].properties.zone == selected_zone) || (d[1].properties.zone == selected_zone)){
+                      return false;
+                    }
+                    else{
+                      return true;
+                    }
+                  });
+                  zone_toggle = true;
+                }
+          });
+          
+
+         // firstflag - 0(don't animate) 1(animate)
+          function datacaller(data,firstflag){
+
+            // data filter
+            var data_range = selected_data.split('-');
+            data = data.filter(function(d){
+                  return ((parseInt(d[0].properties.rowno) >= parseInt(data_range[0])) 
+                            && (parseInt(d[0].properties.rowno) <= parseInt(data_range[1])));
+              });   
+            
+              if(selected_class!= "All"){
+                data = data.filter(function(d){
+                  return d[0].properties.subscriber_class == selected_class;
+                });
+              }
+
+              if(selected_purpose!= "All"){
+                data = data.filter(function(d){
+                  return d[0].properties.purpose == selected_purpose;
+                });
+              }
+                
+                  //remove previous elements
+                  d3.selectAll('.lineConnect').remove();
+                  d3.selectAll('.travelMarker')
+                      .transition().duration(1000)
+                      .attr('r',0)
+                      .remove();
+                  
+                  // invisible line for animation
+                  var lp = g.selectAll(".lineConnect")
+                      .data(data);
+                    
+                    
+                  var linePath = lp.enter().append("path")
+                    .attr("class", "lineConnect")
+                    .attr('id',function(d,i){return "line_"+i});
+                  
+                  // travelling circle marker  (debug this)      
+                  var mr = g.selectAll('travelMarker')
+                          .data(data)
+                          
+                  var marker = mr.enter().append('g')
+                            .attr("class", "travelMarker")
+                            .attr("id", function(d,i){ return "marker_"+i});
+                  
+    
+                  var maxvalue = d3.max(data,function(d){return parseFloat(d[0].properties.radius)});
+                  var minvalue = d3.min(data,function(d){return parseFloat(d[0].properties.radius)});
+                  
+                  // create scale for radius
+                  var radius_scale = d3.scale.linear()
+                                        .domain([minvalue,maxvalue])
+                                        .range([4,10]);
+
+                  // outer pulse         
+                  marker.append("circle")
+                          .attr("class", "travelMarker")
+                          .attr('r',0).classed("pulse",true)
+                          .style({'stroke': '#6495ED', 'stroke-width': 8, 'fill': null}) 
+                          .transition().duration(1000)
+                          .attr('r',function(d){
+                            return radius_scale(parseFloat(d[0].properties.radius)) ;
+                          });
+
+                  // inner circle
+                  marker.append('circle').attr('r',0)
+                          .attr("class", "travelMarker")
+                          .style({'stroke': '#6495ED', 'stroke-width': 1, 'fill': '#EEC900'})
+                          .transition().duration(1000)
+                          .attr('r',function(d){
+                              return radius_scale(parseFloat(d[0].properties.radius));
+                          })
+                          .attr('opacity',1);
+                  
+           
+                // function to continue filtering after time increment
+                  function toggle_continuer(){
+                    if((zone_toggle == true) && (selected_zone != 0)){
+                      d3.selectAll('.travelMarker').classed('hidden',function(d){
+                                if((d[0].properties.zone == selected_zone) || (d[1].properties.zone == selected_zone)){
+                                  return false;
+                                }
+                                else{
+                                  return true;
+                                }
+                              });
+                    }
+                  }
+
+                  toggle_continuer();
+                 
+                map.on("viewreset", reset);
+                  reset();
+                  
+                function reset() {
+                      var bounds = path.bounds(geodata),
+                          topLeft = bounds[0],
+                          bottomRight = bounds[1];
+
+                      svg.attr("width", bottomRight[0] - topLeft[0])
+                          .attr("height", bottomRight[1] - topLeft[1])
+                          .style("left", topLeft[0] + "px")
+                          .style("top", topLeft[1] + "px");
+
+                      g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+                      paths.attr("d", path);
+                      
+                      circlefeat.attr("transform", 
+                        function(d) { 
+                          return "translate("+ 
+                            map.latLngToLayerPoint(d.LatLng).x +","+ 
+                            map.latLngToLayerPoint(d.LatLng).y +")";
+                          }
+                        )
+                  
+                       marker.attr("transform",
+                                  function(d) {
+                                      var y = d[0].geometry.coordinates[1];
+                                      var x = d[0].geometry.coordinates[0];
+                                      return "translate(" +
+                                          map.latLngToLayerPoint(new L.LatLng(y, x)).x + "," +
+                                          map.latLngToLayerPoint(new L.LatLng(y, x)).y + ")";
+                                  });
+
+                      linePath.attr('d',toLine);
+                }
+                 function transition() {
+
+                      d3.select(this).transition().duration(6500)
+                        .delay(1000)
+                        .attrTween("stroke-dasharray",tweenDash);
+                         
+                  } 
+                  function tweenDash() {
+                      // simultaneous transition
+                      var current = this;
+                      return function(t) {
+                          lineid = current.id.split("_")[1];
+                          var l = current.getTotalLength();
+                          interpolate = d3.interpolateString("0," + l, l + "," + l);
+                          var marker = d3.select("#marker_" + lineid);
+                          var p = current.getPointAtLength(t * l);
+                          marker.attr("transform", "translate(" + p.x + "," + p.y + ")");
+                          return interpolate(t);
+                      }
+
+                  } 
+
+                  // transition when firstflag = 1
+                  if(firstflag){
+                    linePath.each(transition);
+                  }
+
+          }
+
+          // initial time index
+          index = 4;
+          var prev_index = 0;
+
+          //initial call for show
+          datacaller(data[index],0);
+
+          dropDownclass.on('change',function(d){
+                  selected_class = d3.event.target.value;
+                  if(playing == false){
+                    datacaller(data[index],0);
+                  }
+                  
+          });
+
+          dropDownpurpose.on('change',function(d){
+                  selected_purpose = d3.event.target.value;
+                  if(playing == false){
+                    datacaller(data[index],0);
+                  }
+                  
+          });
+
+          dropDownTime.on('change',function(){
+                prev_index = parseInt(index);
+                index = d3.event.target.value;
+                timetext.text("Time : "+index);
+                if(playing == false){
+                  datacaller(data[index],0);
+                }
+                
+          });
+
+        dropDowndata.on('change',function(){
+                    selected_data = d3.event.target.value;
+                    if(playing == false){
+                      datacaller(data[index],0);
+                    }
+                    
+                    
+              });
+
+
+           
+        var textsvg = d3.select('body').append('svg')
+                  .attr('class','timetext')
+                  .attr('width',200).attr('height',200);
+                        
+        var timetext = textsvg.append('text')
+                        .attr('class','timetext')
+                        .attr('x',100).attr('y',100)
+                        .style("font-size", "26px") 
+                        .text("Time : " + index);
+
+        //var t;
+
+        function animateMap(){
+          if(playing == false){
+            //initial animation
+            datacaller(data[index],1);
+            d3.select(this).html('pause');
+            playing = true;
+             t = d3.interval(function(){
+                
+                /*
+                if(stopflag == true){
+                  stopflag = false;
+                  t.stop();
+                }
+                */
+
+                index++;
+
+                // not stopping during animation
+                dropDownTime.on('click',function(d){
+                  var prev_index = index;
+                  index = d3.event.target.value;
+                  // decrease to show the clicked index
+                  if(playing == true){
+                    index--;
+                  }
+                  if(playing == false){
+                    datacaller(data[index],0);
+                  }
+                  
+                });
+
+                
+                if(index < data.length){
+                  datacaller(data[index],1);
+                }
+                else{
+                  d3.select('#play').html('play');
+                  playing = false;
+                  t.stop();
+                  index = 0;
+                }
+                timetext.text("Time : " + index);
+
+          },8000);
+
+
+          }
+          else{
+            if(t){
+              t.stop();
+            }
+            index++;
+            d3.select(this).html('play');
+            playing = false;
+          }
+          timetext.text("Time : " + index);
+
+        }
+
+        d3.select('#play').on('click',animateMap);
+
+        
+        function applyLatLngToLayer(d) {
+            var y = d.geometry.coordinates[0];
+            var x = d.geometry.coordinates[1];
+            return map.latLngToLayerPoint(new L.LatLng(x,y))
+          }
+          
+        function projectPoint(x, y) {
+            var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+            this.stream.point(point.x, point.y); 
+        }  
+
+}
+
+
+
---
+++ js/fileexp.js
@@ -0,0 +1,276 @@
+// drop down for day
+var dropDowndate = d3.select('#date');
+
+var date_list = d3.range(1,31);
+var dateoptions = dropDowndate.selectAll('.dateoptions')
+					.data(date_list)
+					.enter()
+					.append('option')
+					.attr('class','dateoptions')
+
+dateoptions.text(function (d) { return d; })
+       .attr("value", function (d) { return d; });
+
+
+var selected_date = 1;
+
+// drop down for county
+var dropDown = d3.select('#county-list');
+
+var county_list = ["All","ORANGE","SEMINOLE","OSCEOLA","VOLUSIA","BREVARD","INDIANRIVER","LAKE","SUMTER","POLK"];
+
+var options = dropDown.selectAll(".countyoption")
+           	.data(county_list)
+         	.enter()
+           	.append("option")
+           	.attr('class','countyoption');
+
+options.text(function (d) { return d; })
+       .attr("value", function (d) { 
+       		if(d == 'All'){
+       			return "";
+       		}
+       		return d; });
+
+var selectedValue = "";
+
+
+// drop down for time
+var dropDownTime = d3.select("#time-hour");
+
+var hours_list = d3.range(24);
+
+var time_options = dropDownTime.selectAll(".timeoption")
+           	.data(hours_list)
+         	.enter()
+           	.append("option").attr('class',"timeoption");
+
+time_options.text(function (d) { return d; })
+       .attr("value", function (d) { return d; });
+
+
+// drop down for subscriber class
+
+var dropDownclass = d3.select('#subscriber-class'); 
+
+var subscriber_class = ["All","Home Worker", "Resident Worker", "Inbound Commuter", "Outbound Commuter", "Short Term Visitor", "Long Term Visitor"];
+
+var class_options = dropDownclass.selectAll(".classoption")
+           	.data(subscriber_class)
+         	.enter()
+           	.append("option").attr('class',"classoption");
+
+class_options.text(function (d) {return d;})
+       .attr("value", function (d) {return d;});
+
+var selected_class = "All";
+
+
+// drop down for purpose
+var dropDownpurpose = d3.select('#purpose'); 
+
+var purpose_list = ["All","HW", "WH", "HO", "OH", "OW", "WO", "HH", "OO", "WW"];
+
+var purpose_options = dropDownpurpose.selectAll(".purposeoption")
+           	.data(purpose_list)
+         	.enter()
+           	.append("option").attr('class',"purposeoption");
+
+purpose_options.text(function (d) {return d;})
+       .attr("value", function (d) {return d;});
+
+var selected_purpose = "All";
+
+// drop down for selecting data subset
+var dropDowndata = d3.select('#select-data'); 
+
+var selectdata = [],
+	mod = 30,
+	i = 1,
+	num = parseInt(150000/mod);
+
+var selected_data = "" + i + "-" + (i + num - 1);
+
+while(i <= 150000){
+	if((i + num - 1) < 150000){
+		selectdata.push("" + i + "-" + (i + num - 1));
+	}
+	else{
+		selectdata.push("" + i + "-" + "150000");
+	}
+	i = i + num ;
+}
+
+var data_options = dropDowndata.selectAll(".dataoption")
+           	.data(selectdata)
+         	.enter()
+           	.append("option").attr('class',"dataoption");
+
+data_options.text(function (d) {return d;})
+       .attr("value", function (d) {return d;});
+
+
+// play button 
+var playing = false;
+
+// centroid data
+var zonecentroid = {};
+var geodata = [];
+var circledata = {};
+circledata["objects"] = [];
+
+// create map
+var map = L.map('map').setView([28.54136, -81.36053], 10);
+
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png').addTo(map);
+
+
+var svg = d3.select(map.getPanes().overlayPane).append("svg"),
+      g = svg.append("g").attr("class", "leaflet-zoom-hide");
+
+//create data for animation
+var final_list = [];
+
+dropDowndate.on('change',function(){
+        selected_date = d3.event.target.value;
+        menuChanged();
+    });
+
+dropDown.on("change", function(){
+	selectedValue = d3.event.target.value;
+	menuChanged(selectedValue);
+} );
+
+menuChanged("first_call");
+
+// stop when drop down for county is selected
+var stopflag = false;
+
+var t;
+
+function menuChanged(d){
+
+	// Remove all elements
+	d3.selectAll('.travelMarker').remove();
+	d3.selectAll('.lineConnect').remove();
+	d3.selectAll('.regioncenter').remove();
+	d3.selectAll('.timetext').remove();
+	d3.selectAll('path').remove();
+	
+
+	circledata["objects"] = [];
+
+	
+	// default orange county
+	if((d!="first_call") && (d!="class_change") && (playing == true)){
+		//selectedValue = d3.event.target.value;
+		// stop for county
+		stopflag = true;
+		if(county_list.indexOf(d) > -1){
+			t.stop();
+		}
+		
+	}
+
+
+	d3.select('#play').html('play');
+            playing = false;
+
+	final_list = [];
+
+	var geoload = "Data/GeoJSONDataByCounties/" + "OrlandoGeoJ_" + selectedValue + ".json";
+
+	// load geojson
+	d3.json(geoload, function (error, data){
+	            geodata = data;
+	            //get centroid
+	            data.features.forEach(function (d, i) {
+	            	var temp_dict = {};
+	            	temp_dict["circle"] = {}
+	                var centroid = d3.geo.centroid(d.geometry);
+	                zonecentroid[d.properties.TAZ_ID] = centroid;
+	                temp_dict['circle']['tazid'] = d.properties.TAZ_ID;
+	                temp_dict['circle']['coordinates'] = centroid;
+	                circledata['objects'].push(temp_dict);
+	            });
+	            zmain();
+	        });
+
+
+	function zmain(){
+
+		var matrixload = "Data/Travel_Data/" + "matrix" + selectedValue + "_" + selected_date + ".csv";
+		// load csv
+		d3.csv(matrixload,function(data){
+
+			var hourlydata = [];
+
+			for(var hour = 0;hour<24;hour++){
+				hourlydata.push(data.filter(function(d)
+					{return d.Start_time == hour;}));
+			}
+
+			for(var hour = 0;hour<24;hour++){
+				//create a list of dictionaries for each route
+
+				//no value
+				if(!hourlydata[hour]){
+					continue;
+				}
+
+				var timely_list = [];
+
+				for(var i in d3.range(hourlydata[hour].length)){
+					var temp_dict = {}
+					temp_dict["features"] = [];
+					for(var j in d3.range(2)){
+						var inner_dict = {};
+						inner_dict["properties"] = {};
+						inner_dict["geometry"] = {};
+						inner_dict["geometry"]["type"] = "Point";
+						if(j==0){
+							// row number
+							inner_dict['properties']['rowno'] =  hourlydata[hour][i]['Rowno'];
+							// subscriber class
+							inner_dict['properties']['subscriber_class'] = hourlydata[hour][i]['Subscriber_Class'];
+							// purpose
+							inner_dict['properties']['purpose'] = hourlydata[hour][i]['Purpose'];
+							// radius
+							inner_dict["properties"]["radius"] = hourlydata[hour][i]["Count"];
+							// get coordinates of origin
+							var origin_zone = hourlydata[hour][i]["Origin_Zone"];
+							inner_dict['properties']['zone'] = origin_zone;
+							inner_dict["geometry"]["coordinates"] = zonecentroid[origin_zone];
+						}
+						else{
+							// row number
+							inner_dict['properties']['rowno'] =  hourlydata[hour][i]['Rowno'];
+							//subscriber class
+							inner_dict['properties']['subscriber_class'] = hourlydata[hour][i]['Subscriber_Class'];
+							// purpose
+							inner_dict['properties']['purpose'] = hourlydata[hour][i]['Purpose'];
+							// radius
+							inner_dict["properties"]["radius"] = hourlydata[hour][i]["Count"];
+							// get coordinates of destination
+							var destination_zone = hourlydata[hour][i]["Destination_Zone"];
+							inner_dict['properties']['zone'] = destination_zone;
+							inner_dict["geometry"]["coordinates"] = zonecentroid[destination_zone];
+						}
+
+						temp_dict['features'].push(inner_dict);
+					}
+					
+					timely_list.push(temp_dict);
+				}
+				final_list.push(timely_list);
+			}
+			mainmapcall();
+		})
+
+	}
+
+}
+
+
+
+
